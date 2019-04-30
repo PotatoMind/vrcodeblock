8f7f1aa8564f7a2db5ae79671f7580a5c93f3b50
Aaron <hammer.21@wright.edu>
2019-04-29 21:12:05 -0400

    Initial commit

---
+++ Assets/DragBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragBehavior : MonoBehaviour
+{
+    public Color draggingColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+ 
+    void Start() 
+    {
+        originalColor = GetComponent<Renderer>().material.color;
+    }
+ 
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        GetComponent<Renderer>().material.color = draggingColor;
+        dragging = true;
+    }
+ 
+    void OnMouseUp()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+        dragging = false;
+    }
+ 
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
---
+++ Assets/Spin.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Whether the Google Cardboard user is gazing at this button.
+    private bool isLookedAt = false;
+
+    // Has the object been picked up?
+    private bool isPickedUp = false;
+ 
+    // How long the user can gaze at this before the button is clicked.
+    public float timerDuration = 5f;
+
+    public float pickUpStopThreshold = 0.01f;
+
+    private float elapsedTime = 0f;
+    
+    private float pickUpTimer = 0f;
+ 
+    // Count time the player has been gazing at the button.
+    private float lookTimer = 0f;
+
+    // Set to reticle growth speed
+    public float growthSpeed = 2f;
+    
+    // Graphical progress indicator.
+    private GameObject gazeTimer;
+
+    private Vector3 mLastPosition;
+
+    Color color = Color.red;
+ 
+    // Use this for initialization
+    void Start () {
+        gazeTimer = GameObject.Find("GvrReticlePointer");
+        GvrReticlePointer script = gazeTimer.GetComponent<GvrReticlePointer>();
+        if (script) {
+            gazeTimer.GetComponent<GvrReticlePointer>().reticleGrowthSpeed = growthSpeed;
+        }
+        mLastPosition = transform.position;
+        color = GetComponent<Renderer>().material.color;
+    }
+    
+    
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Pressed it at 0");
+        }
+        if (Input.GetButtonDown("Fire1")) {
+            Debug.Log("Pressed it at Fire1");
+        }
+        // While player is looking at this button.
+        if (isLookedAt) {
+            if (isPickedUp) {
+                elapsedTime += Time.deltaTime;
+                mLastPosition = transform.position;
+
+                Ray ray = new Ray(gazeTimer.transform.position, gazeTimer.transform.forward);
+                float dist = Vector3.Distance(gazeTimer.transform.position, transform.position);
+                //Debug.Log(dist);
+                transform.position = ray.GetPoint(dist);
+
+                float speed = (transform.position - this.mLastPosition).magnitude / elapsedTime;
+                //Debug.Log(speed);
+                if (speed < pickUpStopThreshold) {
+                    pickUpTimer += Time.deltaTime * growthSpeed;
+
+                    if (pickUpTimer > timerDuration) {
+                        pickUpTimer = 0f;
+                        elapsedTime = 0f;
+                        isPickedUp = false;
+                        GetComponent<Renderer>().material.color = color;
+                    }
+                }
+            } else {
+                // Increment the gaze timer.
+                lookTimer += Time.deltaTime * growthSpeed;
+    
+                // Gaze time exceeded limit - button is considered clicked.
+                if (lookTimer > timerDuration) {
+                    lookTimer = 0f;
+                    isPickedUp = true;
+                    GetComponent<Renderer>().material.color = Color.grey;
+                }
+            }
+        }
+        // Not gazing at this anymore, reset everything.
+        else {
+            lookTimer = 0f;
+        }
+    }
+
+    // Record whether Google Cardboard user is gazing at the button.
+    // gazedAt: Set it to the value passed from event trigger.
+    public void setGazedAt(bool gazedAt) {
+        isLookedAt = gazedAt;
+    }
+}
